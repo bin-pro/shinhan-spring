f8ef7d44b44a4fed776f4cba2e67f49fcba0629c
System Administrator <root@isubin-ui-MacBookPro.local>
2023-09-25 08:40:02 +0900

    Init

---
+++ src/main/java/com/example/springbootlecture/SpringBootLectureApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootlecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLectureApplication.class, args);
+    }
+
+}
